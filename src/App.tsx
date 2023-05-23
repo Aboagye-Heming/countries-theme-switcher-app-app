6e90b35982045ab3b8aeaa45b027af995297a2b6
Aboagye=Heming <hemingkwabena@gmail.com>
2023-05-23 22:21:17 +0000

    created the nav bar and the search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import flag from "./assets/images/1280px-Flag_of_Germany.png"
+import expand from './assets/images/expand-more.svg'
+import darkbtn from './assets/images/Group3.svg'
+import search from './assets/images/search.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <div className="nav-bar">
+        <h3>Where in the world?</h3>
+        <div className='dark-mode'>
+          <img src={darkbtn} alt="" />
+          <p>Dark Mode</p></div>
+      </div>
+      <div className='filter'>
+        <div className="search">
+          <img src={search} alt="" />
+          <input type="text" placeholder='Search for a country…' />
+        </div>
+        <div className='filter-drop'>
+          <label htmlFor="countries">Filter by Region </label>
+          <select name="countries" id="countries">
+            <option value="Africa">Africa</option>
+            <option value="America">America</option>
+            <option value="Asia">Asia</option>
+            <option value="Asia">Europe</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </div>
+      </div>
+      <div className='countries'>
+        <div className="country">
+          <img src={flag} alt="" />
+          <div className='description'>
+            <h4>Germany</h4>
+            <p><span>Population:</span> 81,770,900</p>
+            <p><span>Region:</span> Europe</p>
+            <p><span>Capital:</span> Berlin</p>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
