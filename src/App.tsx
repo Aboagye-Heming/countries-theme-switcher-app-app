6b4925c1c07d45be4ff5f2aa06d099cf18062cc6
Aboagye=Heming <hemingkwabena@gmail.com>
2023-06-20 21:14:03 +0000

    made fixes to the details page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import { useState } from "react";
-import {
-  BrowserRouter, Routes, Route
-} from "react-router-dom";
-import NavBar from "./components/NavBar"
-import './App.scss';
-import { ThemeContext } from './context/themeContext';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import "./App.scss";
+import ScrollToTop from "./ScrollToTop";
+import { ThemeContext } from "./context/themeContext";
 
-import MainPage from "./pages/MainPage"
-import DetailsPage from "./pages/DetailsPage"
+import MainPage from "./pages/MainPage";
+import DetailsPage from "./pages/DetailsPage";
 
 function App() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState("dark");
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <BrowserRouter>
         <main className={`theme-${theme}`}>
           <NavBar />
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/:code/details" element={<DetailsPage />} />
-          </Routes>
+          <ScrollToTop>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/:code/details" element={<DetailsPage />} />
+            </Routes>
+          </ScrollToTop>
         </main>
       </BrowserRouter>
     </ThemeContext.Provider>
